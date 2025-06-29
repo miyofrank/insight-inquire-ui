2b55ce0e312b048bca13b7c9b1feacabeb14f661
Frank cruz <frank.cruz.alva@gmail.com>
2025-06-29 14:56:57 -0500

    icono

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -103,14 +103,14 @@ const Index = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center space-x-4">
-                <div className="w-24 h-8 bg-white-100 rounded-full flex items-center justify-center overflow-hidden">
-                  <img
-                    src="https://eficientis.com/wp-content/uploads/2024/03/image-18.webp" // Reemplaza con tu URL real
-                    alt="icono"
-                    className="w-5 h-5"
-                  />
-                </div>
-                <span className="text-xl font-semibold text-gray-900">Eficientis encuestas</span>
+              <div className="w-[150px] h-auto">
+                <img
+                  src="https://eficientis.com/wp-content/uploads/2024/03/image-18.webp"
+                  alt="Logo Eficientis"
+                  className="w-full h-auto object-contain"
+                />
+              </div>
+              <span className="text-xl font-semibold text-gray-900">Eficientis encuestas</span>
             </div>
             <Button 
               onClick={() => setShowCreateModal(true)}
