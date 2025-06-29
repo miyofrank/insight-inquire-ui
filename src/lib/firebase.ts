366e82e93e501b8719ebac245beae4e9f09f9221
Frank cruz <frank.cruz.alva@gmail.com>
2025-06-29 18:15:43 -0500

    Se agrego credenciales de acceso a firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from 'firebase/auth';
 
 // TODO: Reemplaza con tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyA8zP01FgvQMFEv16zd6A3lY2xTgFqFj3o",
+  authDomain: "projectsurvey-eedeb.firebaseapp.com",
+  projectId: "projectsurvey-eedeb",
+  storageBucket: "projectsurvey-eedeb.firebasestorage.app",
+  messagingSenderId: "433405392246",
+  appId: "1:433405392246:web:205187436f7de8637ab657",
+  measurementId: "G-V13W6W1WTH"
 };
 
 const app = initializeApp(firebaseConfig);
